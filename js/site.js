28f5291da6e748e70009890658d35456e91a86a4
Greg Perkins <greg@collateralcode.com>
2016-08-22 16:33:51 -0600

    add bootstrap js as well as an integration directive for external code calls

--- js/site.js
+++ js/site.js
@@ -1,4 +1,28 @@
-angular.module('FallHarvestApp', [])
-.controller('FallHarvestController', function() {
+var app = angular.module('FallHarvestApp', []);
+
+///////////////////////////////////////////////////////////////////////////////
+
+app.controller('FallHarvestController', function($scope) {
     var vm = this;
+
+    $scope.$on('someName', function(event) {
+        console.log('event fired:', event);
+    });
+});
+
+///////////////////////////////////////////////////////////////////////////////
+
+// put emit-on-update="someName" on an ng-repeat and it will fire someName
+// after the DOM settles down from rendering
+app.directive('emitOnUpdate', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit(attr.emitOnUpdate);
+                });
+            }
+        }
+    }
 });
