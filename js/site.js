2e5cdf218a5ca896de739ce47c5fb71e7779a65a
Cody Evans <cevans@drakecooper.com>
2016-12-05 10:29:30 -0700

    stylng video section

--- js/site.js
+++ js/site.js
@@ -7,6 +7,8 @@ function pageReady(vm) {
 
     $('.carousel').slick({
       adaptiveHeight: true,
+      prevArrow: '<button type="button" class="prev"><span class="visually-hidden">Previous</span></button>',
+      nextArrow: '<button type="button" class="next"><span class="visually-hidden">Next</span></button>',
     });
     $('.carousel').slick('slickGoTo', vm.currentSlide); // initial slide
 
